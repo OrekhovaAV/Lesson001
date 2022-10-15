3b1b457c83442447b49857c9eaa3537e5f873104
Orekhova Anastasia <Kopatulka1994@yandex.ru>
2022-10-15 20:30:13 +0300

    рассмотрели виды методов, решили задачу упорядочивание массива

---
+++ Exsample012_Metods/Program.cs
@@ -0,0 +1,183 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+// Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2("Текст сообщения"); // или Method2(msg: "Текст сообщения");
+// или
+void Metod21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+// Metod21("Текст", 4); // Ожидаем увидеть 4 раза слово "Текст"//или Metod21(msg: "Текст",count: 4) или Metod21(count: 4, msg: "Текст")
+
+// Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+// Console.WriteLine(year);
+
+// Вид 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; // пустая строка
+
+    while (i < count)
+    {
+        result = result + text;    
+        i++;
+    }
+    return result;
+}
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+string Method41(int count, string text)
+{
+    string result = String.Empty; // пустая строка
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;    
+       
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+//Console.WriteLine(res);
+
+/*
+//вывод таблицы умножения на экран
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i *j}"); 
+    }
+    Console.WriteLine();
+}
+*/
+/*
+// Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить большими "К", а большие "С" заменить маленькими "с".
+string text = "- Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue) // есть какой то текст, есть конкретный символ и новый символ на который будем менять
+// конкретный символ
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'C', 'c');
+Console.WriteLine(newText);
+*/
+
+// Упорядочить массив (поиск минимального элемента в неотсортированной части)
+/*
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+*/
+
+// Упорядочить массив (поиск минимального элемента в неотсортированной части)
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
